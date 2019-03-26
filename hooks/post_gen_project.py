aafc91841ad58dfb85985271c66878f35478e42c
Guillaume Gelin <ramnes@numberly.com>
2019-03-26 14:18:42 +0100

    Force a Python 3.7 virtualenv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,7 +16,7 @@ virtualenv = input("Create virtualenv? [y]:")
 repository = input("Initialize Git repository? [y]:")
 
 if virtualenv == "y" or not virtualenv:
-    os.system("virtualenv .venv")
+    os.system("virtualenv -ppython3.7 .venv")
     requirements = input("Install requirements? [y]:")
     if requirements == "y" or not requirements:
         os.system(".venv/bin/pip install -r dev-requirements.txt")
